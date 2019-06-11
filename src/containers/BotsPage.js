35f99514530b15936b8902aa536e62063ad54a07
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-11 09:55:03 -0400

    Checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,51 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    bots: [],
+    myBots: []
+  }
+
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(r => r.json())
+      .then(bots => this.setState({bots}))
+  }
+
+
+  addToMyBots = e => {
+    const addBotId = parseInt(e.currentTarget.id, 10) // FIND ID OF CLICKED BOT
+    const findBotInMyBots = this.state.myBots.find(bot => bot.id === addBotId)
+
+    if (!findBotInMyBots) { // IF BOT IS NOT IN myBots, ADD TO myBots
+      const findBotInBots = this.state.bots.find(bot => bot.id === addBotId)
+      this.setState({myBots: [...this.state.myBots, findBotInBots]})
+    }
+  }
+
+
+  removeFromMyBots = e => {
+    // FIND ID OF CLICKED BOT AND FILTER ONLY THOSE BOTS WHOSE ID != CLICKED BOT ID
+    const rmvBotId = parseInt(e.currentTarget.id, 10)
+    const myBotsCopy = this.state.myBots.filter(bot => bot.id !== rmvBotId)
+    this.setState({myBots: myBotsCopy})
+  }
+
 
   render() {
+    console.log("App state: ", this.state)
+
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          bots={this.state.myBots}
+          removeFromMyBots={this.removeFromMyBots} />
+        <BotCollection
+          bots={this.state.bots}
+          addToMyBots={this.addToMyBots} />
       </div>
     );
   }
