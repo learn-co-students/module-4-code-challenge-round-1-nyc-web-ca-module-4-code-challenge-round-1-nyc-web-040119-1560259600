dccdbe1be61154164a377e374699ef7c67534671
axionos <axionoso@gmail.com>
2019-06-11 10:10:11 -0400

    deliverable 1-3 done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,60 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  constructor(props){
+    super(props)
+    this.state = {
+      bots: [],
+      army: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(response => response.json())
+    .then(bots => {
+      return this.setState({
+        bots
+      })
+    })
+  }
+
+  addBot = (bot) => {
+    console.log(bot)
+    const copyBots = [...this.state.bots]
+    const newBots = copyBots.filter(theBot => {
+      return theBot.name !== bot.name
+    })
+    this.setState({
+      bots: newBots,
+      army: [...this.state.army, bot]
+    })
+  }
+
+  removeBot = (bot) => {
+    const copyArmy = [...this.state.army]
+    const newArmy = copyArmy.filter(theBot => {
+      return theBot.name !== bot.name
+    })
+    this.setState({
+      bots: [...this.state.bots, bot],
+      army: newArmy
+    })
+  }
 
   render() {
+    console.log('Bots Page State', this.state)
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection
+          bots={this.state.bots}
+          addBot={this.addBot}/>
+        <YourBotArmy
+          bots={this.state.bots}
+          removeBot={this.removeBot}/>
+
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,21 +2,29 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+  renderBots = () => {
+    return this.props.bots.map(bot => {
+      return <BotCard
+        key={bot.id}
+        bot={bot}
+        handleClick={this.props.removeBot}
+        />
+    })
+  }
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.renderBots()}
+
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
